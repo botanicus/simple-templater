82ebce2c0b9e014c1d8c819e888f0f7a446524a0
Jakub Stastny <random@JakubMac.local>
2009-10-28 13:47:08 +0000

    SimpleTemplater::Main

---
+++ lib/simple-templater/main.rb
@@ -0,0 +1,34 @@
+module SimpleTemplater
+  class Main
+    attr_reader :scope, :logger, :generators
+    def initialize(scope, logger = nil)
+      @scope      = scope
+      @logger     = logger
+      @generators = Hash.new
+    end
+    
+    def logger
+      @logger ||= standard_logger
+    end
+
+    def discover!
+      SimpleTemplater.discover!(self.scope)
+    end
+    
+    def generators
+      SimpleTemplater.scopes[self.scope]
+    end
+
+    # templater.register(:project, path)
+    def register(name, path)
+      raise GeneratorNotFound unless File.directory?(path) # TODO: validations of directory content
+      @generators[name] = path
+    end
+    
+    private
+    def standard_logger
+      require "logger"
+      Logger.new(STDOUT)
+    end
+  end
+end
