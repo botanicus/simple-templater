d45d556b9a7a7fe97ba7d9827340ce53ce1e1fa8
Jakub Šťastný aka Botanicus <knava.bestvinensis@gmail.com>
2009-10-31 18:12:55 +0000

    Setup $: for specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 
 SPEC_ROOT = File.dirname(__FILE__)
+$:.unshift(File.expand_path(File.join(SPEC_ROOT, "..", "lib")))
 require "spec" # so you can run ruby spec/rango/whatever_spec.rb
 
 module Spec
