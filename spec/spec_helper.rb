4ec3f823f8c4c93ada242265b43b30eddbdd3f18
Jakub Stastny <random@JakubMac.local>
2009-11-03 15:12:21 +0000

    Require fakefs in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,9 @@
 
 SPEC_ROOT = File.dirname(__FILE__)
 $:.unshift(File.expand_path(File.join(SPEC_ROOT, "..", "lib")))
+
 require "spec" # so you can run ruby spec/rango/whatever_spec.rb
+require "fakefs"
 
 module Spec
   module Matchers
