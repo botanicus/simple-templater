54d48b83a1efffed98216b2da590cc1ece065ca1
Jakub Šťastný aka Botanicus <knava.bestvinensis@gmail.com>
2009-11-18 16:50:58 +0000

    But not the source path, but the content path

--- lib/simple-templater/generator.rb
+++ lib/simple-templater/generator.rb
@@ -47,7 +47,7 @@ module SimpleTemplater
         if File.exist?(hook = File.join(self.path, "preprocess.rb"))
           load hook
         end
-        SimpleTemplater::Templater.create(self.path)
+        SimpleTemplater::Templater.create(file("content"))
       end
       self.run_postprocess_hook
     end
