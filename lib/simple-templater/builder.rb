ee967025aa7da084b1d743b8a735d7ce299228ed
Jakub Šťastný aka Botanicus <knava.bestvinensis@gmail.com>
2009-11-19 11:16:17 +0000

    Support for path/%variable%.txt and path/%collection%.txt

--- lib/simple-templater/builder.rb
+++ lib/simple-templater/builder.rb
@@ -27,7 +27,7 @@ class SimpleTemplater
         file = self.expand_path(template)
         next if template == @content_dir
         if File.file?(template)
-          proceed_file(template, file)
+          proceed(template, file)
         else
           unless Dir.exist?(file)
             SimpleTemplater.logger.debug("[MKDIR] #{file}")
@@ -38,13 +38,12 @@ class SimpleTemplater
     end
 
     protected
-    # replace %variable% in file path
     # %name%_controller.rb => application_controller.rb
     def expand_path(file)
       file = self.location(file).sub(/\.rbt$/, "")
-      @context.each do |key, value|
-        file.gsub!(/%#{Regexp::quote(key)}%/, value.to_s)
-      end
+      # @context.each do |key, value|
+      #   file.gsub!(/%#{Regexp::quote(key)}%/, value.to_s)
+      # end
       return file
     end
 
@@ -52,7 +51,30 @@ class SimpleTemplater
       file.sub(%r[^#{Regexp::quote(@content_dir)}/?], "")
     end
 
-    def proceed_file(template, file)
+    def proceed(template, file)
+      # OK, this is a bit tricky, but it works
+      # == TEMPLATE ==   | == CONTEXT ==             | == OUTPUT ==
+      # config/%user%.rb | {user: "botanicus"}       | config/botanicus.rb
+      # %model%.rb       | {models: ["tag", "post"]} | tag.rb, post.rb
+      if template.match(/%(\w+)%/)
+        singular, plural = $1.to_sym, "#{$1}s".to_sym # TODO: use inflector
+        if self.context.has_key?(singular)
+          target = file.sub(/%(\w+)%/, self.context[singular])
+          proceed_file(template, target)
+        elsif self.context.has_key?(plural)
+          variables = self.context[plural] || Array.new
+          variables.each do |variable|
+            target = file.sub(/%(\w+)%/, variable)
+            proceed_file(template, target, {singular => variable})
+          end
+        else
+          raise "Context #{self.context.inspect} doesn't have key #{singular} or #{plural}"
+        end
+      end
+    end
+
+    def proceed_file(template, file, local_context = Hash.new)
+      SimpleTemplater.logger.debug("Local context: #{local_context.inspect}") unless local_context.empty?
       FileUtils.mkdir_p(File.dirname(file))
       if template.end_with?(".rbt")
         if File.exist?(file)
@@ -62,7 +84,7 @@ class SimpleTemplater
         end
         File.open(file, "w") do |file|
           eruby = Erubis::Eruby.new(File.read(template))
-          context = Erubis::Context.new(@context)
+          context = Erubis::Context.new(@context.merge(local_context))
           context.extend(SimpleTemplater::Helpers)
           begin
             output = eruby.evaluate(context)
