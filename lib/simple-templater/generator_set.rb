f7bdd3657fdb517013b04ab74cb44254641dbb3a
Jakub Šťastný aka Botanicus <knava.bestvinensis@gmail.com>
2009-11-19 16:11:01 +0000

    Provide debug informations about custom generators

--- lib/simple-templater/generator_set.rb
+++ lib/simple-templater/generator_set.rb
@@ -14,8 +14,12 @@ class SimpleTemplater
       @paths = self.custom + check_paths(paths)
     end
 
+    def custom_path
+      File.join(ENV["HOME"], ".#{name}", "stubs")
+    end
+
     def custom
-      directories = Dir["#{ENV["HOME"]}/.#{name}/stubs/*"]
+      directories = Dir["#{self.custom_path}/*"]
       directories.select { |file| Dir.exist?(file) }
     end
 
@@ -24,6 +28,7 @@ class SimpleTemplater
     end
 
     def run(args = ARGV)
+      SimpleTemplater.logger.info "Looking for custom generators in #{self.custom_path}: #{self.custom.inspect}"
       full = self.generators.find { |generator| generator.full? }
       diff = self.generators.find { |generator| generator.diff? }
       raise GeneratorNotFound, "Generator set #{self.inspect} hasn't any full generator" if full.nil?
