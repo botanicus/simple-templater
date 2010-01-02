43ffaa656bd9960a5f92923b3debdb8cb8a581b7
Jakub Stastny aka Botanicus <knava.bestvinensis@gmail.com>
2010-01-02 03:51:35 +0100

    Let's get naked! See http://github.com/botanicus/nake

---
+++ tasks.rb
@@ -0,0 +1,38 @@
+#!./script/nake
+# encoding: utf-8
+
+begin
+  require_relative "gems/environment.rb"
+rescue LoadError
+  abort "You have to install bundler and run gem bundle first!"
+end
+
+ENV["PATH"] = "script:#{ENV["PATH"]}"
+
+require "nake/tasks/gem"
+require "nake/tasks/spec"
+require "nake/tasks/release"
+
+load "code-cleaner.nake"
+
+unless File.exist?(".git/hooks/pre-commit")
+  warn "If you want to contribute to SimpleTemplater, please run ./tasks.rb hooks:whitespace:install to get Git pre-commit hook for removing trailing whitespace"
+end
+
+require_relative "lib/simple-templater"
+
+# Setup encoding, so all the operations
+# with strings from another files will work
+Encoding.default_internal = "utf-8"
+Encoding.default_external = "utf-8"
+
+Task[:build].config[:gemspec] = "simple-templater.gemspec"
+Task[:prerelease].config[:gemspec] = "simple-templater.pre.gemspec"
+Task[:release].config[:name] = "simple-templater"
+Task[:release].config[:version] = SimpleTemplater::VERSION
+
+Nake::Task["hooks:whitespace:install"].tap do |task|
+  task.config[:path] = "script"
+  task.config[:encoding] = "utf-8"
+  task.config[:whitelist] = '(bin/[^/]+|.+\.(rb|rake|nake|thor|task))$'
+end
